d8e1160795cf2061d735527e7ed838adf9d2a571
ranjithsm <119553344+ranjithsm@users.noreply.github.com>
2023-09-19 18:22:41 +0530

    Updated Hero section

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./Hero.css";
 import { HiLocationMarker } from "react-icons/hi";
-
+confirm;
+import CountUp  from "react-countup";
 function Hero() {
   return (
     <section className=" hero-wrapper">
@@ -19,13 +20,38 @@ function Hero() {
             </h1>
           </div>
           <div className="flexColStart hero-des">
-            <span>Find a variety of properties that suit you very easilty</span>
-            <span>Forget all difficulties in finding a residence for you</span>
+            <span className="secondaryText">Find a variety of properties that suit you very easilty</span>
+            <span className="secondaryText">Forget all difficulties in finding a residence for you</span>
           </div>
           <div className="flexCenter search-bar">
             <HiLocationMarker color="var(--blue)" size={25} />
             <input type="text" />
             <button className="button">Search</button>
+          </div>
+
+          <div className="flexCenter stats">
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={8800} end={9000} duration={4}/>
+              <span>+</span>
+              </span>
+              <span className="secondaryText">Premium Products</span>
+            </div>
+            
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp start={1950} end={2000} duration={4}/>
+              <span>+</span>
+              </span>
+              <span className="secondaryText">Happy Customers</span>
+            </div>
+            <div className="flexColCenter stat">
+              <span>
+                <CountUp  end={28} />
+              <span>+</span>
+              </span>
+              <span className="secondaryText">Award Winning</span>
+            </div>
           </div>
         </div>
         {/* rightSide */}
